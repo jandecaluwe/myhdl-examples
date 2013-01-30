86df857384109cb91696d5308d68e2a7cca09cb5
Jan Decaluwe <jan@jandecaluwe.com>
2013-01-30 17:30:53 +0100

    lowercase issue

--- gray_counter/gray_counter.py
+++ gray_counter/gray_counter.py
@@ -2,16 +2,16 @@ from myhdl import *
 
 def gray_counter(gray_count, enable, clock, reset):
 
-    N = len(gray_count)
-    gray = Signal(intbv(0)[N:])
+    n = len(gray_count)
+    gray = Signal(intbv(0)[n:])
     even = Signal(bool(1))
 
     @always_seq(clock.posedge, reset=reset)
     def seq():
-        word = concat('1', gray[N-2:], even)
+        word = concat('1', gray[n-2:], even)
         if enable:
             toggled = False
-            for i in range(N):
+            for i in range(n):
                 if word[i] == 1 and not toggled:
                     gray.next[i] = not gray[i]
                     toggled = True
