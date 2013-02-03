40e92e78c19846ee24b6512925090c3d59a621b8
Jan Decaluwe <jan@jandecaluwe.com>
2013-02-03 17:16:56 +0100

    renamed toggled to found, as in the article ...

--- gray_counter/gray_counter.py
+++ gray_counter/gray_counter.py
@@ -10,11 +10,11 @@ def gray_counter(gray_count, enable, clock, reset):
     def seq():
         word = concat('1', gray[n-2:], even)
         if enable:
-            toggled = False
+            found = False
             for i in range(n):
-                if word[i] == 1 and not toggled:
+                if word[i] == 1 and not found:
                     gray.next[i] = not gray[i]
-                    toggled = True
+                    found = True
             even.next = not even
 
     @always_comb
